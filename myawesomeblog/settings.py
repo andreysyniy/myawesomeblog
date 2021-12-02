22b9f4df78b80fb44e087f5af591d4e7c8a68bab
Andres S <andreysyniy@gmail.com>
2021-12-02 18:29:43 +0200

    Add Media

--- myawesomeblog/settings.py
+++ myawesomeblog/settings.py
@@ -122,7 +122,7 @@ STATIC_URL = '/static/'
 
 MEDIA_ROOT = BASE_DIR / 'media'
 
-MEDIA_URL = '/media/'
+MEDIA_URL = '/my_media/'
 
 # Default primary key field type
 # https://docs.djangoproject.com/en/3.2/ref/settings/#default-auto-field
